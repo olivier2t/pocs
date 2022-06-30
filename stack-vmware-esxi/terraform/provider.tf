466e78cfbfe5cb8db6bd8b242b6ab727b72a44a6
Olivier de Turckheim <olivier@Oliviers-MacBook-Pro.local>
2022-06-30 11:51:46 +0200

    fix provider

--- stack-vmware-esxi/terraform/provider.tf
+++ stack-vmware-esxi/terraform/provider.tf
@@ -1,7 +1,7 @@
 provider "vsphere" {
   user           = var.esxi_user
   password       = var.esxi_password
-  esxi_server    = var.esxi_server
+  vsphere_server = var.esxi_server
 
   # If you have a self-signed cert
   allow_unverified_ssl = var.esxi_allow_unverified_ssl
