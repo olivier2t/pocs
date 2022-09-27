5f085d3b7d3060c2a20132a15fc549879d7bb885
Olivier de Turckheim <olivier@Oliviers-MacBook-Pro.local>
2022-09-27 23:29:51 +0200

    fix outputs

--- stack-azure-storage/terraform/module-storage-account/output.tf
+++ stack-azure-storage/terraform/module-storage-account/output.tf
@@ -2,6 +2,6 @@ output "stg_act_name_out" {
     value = resource.azurerm_storage_account.this.name
 }
 
-output "stg_act_name_out" {
-    value = module.rg.stg_act_name_out
+output "rg_name_out" {
+    value = module.rg.rg_name_out
 }
