8eb723f1ab8b8fa380a677b02c1119c1a3c96723
Olivier de Turckheim <olivier@Oliviers-MacBook-Pro.local>
2022-12-11 00:13:18 +0100

    fix tf main path

--- stack-nexus-repository/terraform/aws/variables.tf
+++ stack-nexus-repository/terraform/aws/variables.tf
@@ -1,5 +1,14 @@
 # Cycloid variables
-variable "customer" {}
-variable "project" {}
 variable "env" {}
+variable "project" {}
+variable "customer" {}
+
+# AWS variables
+variable "aws_cred" {
+  description = "Contains AWS access_key and secret_key"
+}
+variable "aws_region" {
+  description = "AWS region where to create servers."
+  default     = "eu-west-1"
+}
 variable "keypair" {}
