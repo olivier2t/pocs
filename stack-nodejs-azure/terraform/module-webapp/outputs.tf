88b509c713c0a7d966d41f59680776a7418ed467
Olivier de Turckheim <olivier@Oliviers-MacBook-Pro.local>
2022-12-13 16:14:47 +0100

    add stacks for azure and gcp

--- stack-nodejs-azure/terraform/module-webapp/outputs.tf
+++ stack-nodejs-azure/terraform/module-webapp/outputs.tf
@@ -3,7 +3,7 @@
 #
 output "rg_name" {
   description = "The name for the Resource Group"
-  value       = data.azurerm_resource_group.webapp.name.
+  value       = data.azurerm_resource_group.webapp.name
 }
 
 #
