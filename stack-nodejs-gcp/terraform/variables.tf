d1917096576390cf60a80f180aa276cda06c9894
Olivier de Turckheim <olivier@Oliviers-MacBook-Pro.local>
2022-12-14 13:20:54 +0100

    add stacks for azure and gcp

--- stack-nodejs-gcp/terraform/variables.tf
+++ stack-nodejs-gcp/terraform/variables.tf
@@ -9,4 +9,8 @@ variable "gcp_region" {
   description = "GCP region where to create servers."
   default     = "europe-west1"
 }
+variable "gcp_zone" {
+  description = "GCP zone where to create servers."
+  default     = "b"
+}
 variable "keypair_public" {}
