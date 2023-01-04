5f727e8b2aa32563e0692ff9dd140d6ba9c11e52
Olivier de Turckheim <olivier@Oliviers-MacBook-Pro.local>
2023-01-04 11:46:57 +0100

    fix provider crdentials

--- stack-nodejs-vmware/terraform/provider.tf
+++ stack-nodejs-vmware/terraform/provider.tf
@@ -1,7 +1,7 @@
 provider "vsphere" {
-  vsphere_server = var.vsphere_env["server"]
-  user           = var.vsphere_env["user"]
-  password       = var.vsphere_env["password"]
+  vsphere_server = var.vsphere_env.server
+  user           = var.vsphere_env.user
+  password       = var.vsphere_env.password
 
   # If you have a self-signed cert
   allow_unverified_ssl = var.vsphere_allow_unverified_ssl
