e3aa3f557aa4915ff57d36ac446fa08ea2a82066
Olivier de Turckheim <olivier@Oliviers-MacBook-Pro.local>
2023-02-17 22:20:40 +0100

    change RDS cluster

--- stack-cmacgm-asg-rds/terraform/module-rds/rds.tf
+++ stack-cmacgm-asg-rds/terraform/module-rds/rds.tf
@@ -4,38 +4,16 @@ resource "random_password" "password" {
   override_special = "!#$%&*()-_=+[]{}<>:?"
 }
 
-# resource "aws_security_group" "pgsql" {
-#   vpc_id      = var.vpc_id
-#   name        = "pgsql"
-#   description = "Allow all inbound for PostgreSQL"
-#   ingress {
-#     from_port   = 5432
-#     to_port     = 5433
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#     # cidr_blocks = [var.vpc_public_subnets]
-#   }
-# }
-
-resource "aws_db_instance" "rds" {
-  identifier             = "rds-${var.rds_engine}-${var.customer}-${var.project}-${var.env}"
-  instance_class         = var.rds_instance_class
-  allocated_storage      = var.rds_allocated_storage
-  engine                 = var.rds_engine
-  username               = var.rds_username
-  password               = random_password.password.result
-  db_name                = var.rds_database_name
-  db_subnet_group_name   = aws_db_subnet_group.rds.name
-  availability_zone      = var.vpc_azs[0]
-  publicly_accessible    = true
-  skip_final_snapshot    = true
-  apply_immediately      = true
-
-  tags = merge(local.merged_tags, {
-    Name        = "rds-${var.rds_engine}-${var.customer}-${var.project}-${var.env}"
-    Environment = var.env
-    role        = "rds"
-  })
+resource "aws_security_group" "rds" {
+  vpc_id      = var.vpc_id
+  name        = "rds-sg"
+  description = "Allow all inbound for PostgreSQL"
+  ingress {
+    from_port   = 5432
+    to_port     = 5433
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_db_subnet_group" "rds" {
@@ -47,6 +25,30 @@ resource "aws_db_subnet_group" "rds" {
     role       = "subnetgroup"
   })
 }
+
+resource "aws_db_instance" "rds" {
+  identifier             = "rds-${var.rds_engine}-${var.customer}-${var.project}-${var.env}"
+  instance_class         = var.rds_instance_class
+  allocated_storage      = var.rds_allocated_storage
+  engine                 = var.rds_engine
+  username               = var.rds_username
+  password               = random_password.password.result
+  db_name                = var.rds_database_name
+  vpc_security_group_ids = [aws_security_group.rds.id]
+  db_subnet_group_name   = aws_db_subnet_group.rds.name
+  availability_zone      = var.azs[0]
+  multi_az               = true
+  publicly_accessible    = true
+  skip_final_snapshot    = true
+  apply_immediately      = true
+
+  tags = merge(local.merged_tags, {
+    Name        = "rds-${var.rds_engine}-${var.customer}-${var.project}-${var.env}"
+    Environment = var.env
+    role        = "rds"
+  })
+}
+
 
 # resource "aws_rds_cluster" "rds" {
 #   cluster_identifier      = "rds-${var.rds_engine}-${var.customer}-${var.project}-${var.env}"
