d14c25fb6d2c147d7018e17e7f361cfcdb175e38
Olivier de Turckheim <olivier@Oliviers-MacBook-Pro.local>
2023-02-17 22:49:07 +0100

    change RDS cluster

--- stack-cmacgm-asg-rds/terraform/module-rds/rds.tf
+++ stack-cmacgm-asg-rds/terraform/module-rds/rds.tf
@@ -36,7 +36,6 @@ resource "aws_db_instance" "rds" {
   db_name                = var.rds_database_name
   vpc_security_group_ids = [aws_security_group.rds.id]
   db_subnet_group_name   = aws_db_subnet_group.rds.name
-  availability_zone      = var.vpc_azs[0]
   multi_az               = true
   publicly_accessible    = true
   skip_final_snapshot    = true
