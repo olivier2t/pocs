1608da881fff914b056ec1c21012aeac8762a260
Olivier de Turckheim <olivier@Oliviers-MacBook-Pro.local>
2023-02-18 09:24:00 +0100

    change RDS cluster

--- stack-cmacgm-asg-rds/terraform/outputs.tf
+++ stack-cmacgm-asg-rds/terraform/outputs.tf
@@ -1,28 +1,28 @@
 #
 # RDS instance outputs
 #
-output "rds_address" {
-  description = "Address for the RDS instance"
-  value       = module.rds.rds_address
-}
+# output "rds_address" {
+#   description = "Address for the RDS instance"
+#   value       = module.rds.rds_address
+# }
 
-output "rds_port" {
-  description = "Port for the RDS instance"
-  value       = module.rds.rds_port
-}
+# output "rds_port" {
+#   description = "Port for the RDS instance"
+#   value       = module.rds.rds_port
+# }
 
 output "rds_db_name" {
   description = "Name for an automatically created database on cluster creation"
   value       = module.rds.rds_db_name
 }
 
-output "rds_username" {
-  description = "The database master username"
-  value       = module.rds.rds_username
-}
+# output "rds_username" {
+#   description = "The database master username"
+#   value       = module.rds.rds_username
+# }
 
-output "rds_password" {
-  description = "The database master password"
-  value       = module.rds.rds_password
-}
+# output "rds_password" {
+#   description = "The database master password"
+#   value       = module.rds.rds_password
+# }
 
