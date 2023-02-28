246024da417e2325a831c5b1fe20cc494ad95784
Olivier de Turckheim <olivier@Oliviers-MacBook-Pro.local>
2023-02-28 23:04:05 +0100

    Minor changes

--- stack-cmacgm-asg-rds/terraform/module-asg/alb.tf
+++ stack-cmacgm-asg-rds/terraform/module-asg/alb.tf
@@ -1,7 +1,7 @@
-data "aws_acm_certificate" "issued" {
-  domain   = "*.shared-services-noprd.aws.cld.cma-cgm.com"
-  statuses = ["ISSUED"]
-}
+# data "aws_acm_certificate" "issued" {
+#   domain   = "*.shared-services-noprd.aws.cld.cma-cgm.com"
+#   statuses = ["ISSUED"]
+# }
 
 resource "aws_lb" "alb" {
   name               = "${var.customer}-${var.project}-${var.env}-alb"
@@ -39,9 +39,11 @@ resource "aws_lb_listener" "alb" {
 
 resource "aws_lb_listener" "alb_ssl" {
   load_balancer_arn = aws_lb.alb.arn
-  port              = "443"
-  protocol          = "HTTPS"
-  certificate_arn   = data.aws_acm_certificate.issued.arn
+  # port              = "443"
+  port              = "80"
+  # protocol          = "HTTPS"
+  protocol          = "HTTP"
+  # certificate_arn   = data.aws_acm_certificate.issued.arn
 
   default_action {
     target_group_arn = aws_lb_target_group.alb.arn
