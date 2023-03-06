47143ede16554a3fc097a6ece0f73d6cb7129485
Olivier de Turckheim <olivier@Oliviers-MacBook-Pro.local>
2023-03-06 13:39:46 +0100

    Changes in k8s stack

--- stack-drupal-k8s/terraform/provider.tf
+++ stack-drupal-k8s/terraform/provider.tf
@@ -1,13 +1,13 @@
 provider "kubernetes" {
   host                   = var.k8s_host
-  cluster_ca_certificate = base64decode(var.k8s_cluster_ca_certificate)
+  cluster_ca_certificate = var.k8s_cluster_ca_certificate
   token                  = var.k8s_token
 }
 
 provider "helm" {
   kubernetes {
     host                   = var.k8s_host
-    cluster_ca_certificate = base64decode(var.k8s_cluster_ca_certificate)
+    cluster_ca_certificate = var.k8s_cluster_ca_certificate
     token                  = var.k8s_token
   }
 }
