26ff37f4cecb279c248ce079e2bf82ee66e59661
Olivier de Turckheim <olivier@Oliviers-MacBook-Pro.local>
2023-03-06 10:11:43 +0100

    Changes in creds

--- stack-harbor-k8s/terraform/variables.tf
+++ stack-harbor-k8s/terraform/variables.tf
@@ -6,13 +6,19 @@ variable "customer" {}
 #
 # Kubernetes
 #
-variable k8s_cluster {
-  description = "Kubernetes Cluster and Credentials. Includes host, cluster_ca_certificate and token."
-  type = object({
-    host                    = string
-    cluster_ca_certificate  = string
-    token                   = string 
-  })
+variable "k8s_host" {
+  description = "The hostname (in form of URI) of the Kubernetes API."
+  default = ""
+}
+
+variable "k8s_cluster_ca_certificate" {
+  description = "PEM-encoded root certificates bundle for TLS authentication."
+  default = ""
+}
+
+variable "k8s_token" {
+  description = "Token of your service account."
+  default = ""
 }
 
 variable "k8s_namespace" {
