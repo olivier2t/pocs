d3733bd66ad773ee3da0eae50587a2509fdb7c15
Olivier <olivier3t@gmail.com>
2023-04-22 22:10:37 +0200

    add terraform for cost estiamtion

---
+++ stack-gitlab-pp/terraform/main.tf
@@ -0,0 +1,33 @@
+module "ec2" {
+  #####################################
+  # Do not modify the following lines #
+  source   = "github.com/olivier2t/iac-library/ec2/module-ec2"
+  project  = var.project
+  env      = var.env
+  customer = var.customer
+  #####################################
+
+  #. extra_tags (optional): {}
+  #+ Dict of extra tags to add on resources. format { "foo" = "bar" }.
+  extra_tags = {
+    demo = true
+    monitoring_discovery = false
+  }
+
+  #
+  # Instance
+  #
+
+  #. vm_instance_type: 't3.micro'
+  #+ Instance type for the VM
+  vm_instance_type = 't3.micro'
+
+  #. vm_disk_size: 20
+  #+ Disk size for the VM (Go)
+  vm_disk_size = 20
+
+  #. keypair_public: ''
+  #+ Public key to provision to the instance
+  keypair_public = var.keypair_public
+
+}
