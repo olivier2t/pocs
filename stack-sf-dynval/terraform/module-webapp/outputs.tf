ad255ced500ef9244c87f358a7f6ce45d4589f79
Olivier de Turckheim <olivier@Oliviers-MacBook-Pro.local>
2023-06-28 13:55:50 +0200

    adding stackforms dyn values stack

---
+++ stack-sf-dynval/terraform/module-webapp/outputs.tf
@@ -0,0 +1,20 @@
+#
+# VPC outputs
+#
+output "vpc_id" {
+  description = "The VPC ID for the VPC"
+  value       = module.vpc.vpc_id
+}
+
+#
+# Instance outputs
+#
+output "vm_ip" {
+  description = "The IP address the EC2 instance"
+  value       = aws_instance.webapp.public_ip
+}
+
+output "vm_os_user" {
+  description = "Admin username to connect to instance via SSH"
+  value       = var.vm_os_user
+}
