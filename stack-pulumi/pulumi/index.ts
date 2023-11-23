6f96331cc39d4201b4070c161dce81fc820515e5
Olivier de Turckheim <olivier@Oliviers-MacBook-Pro.local>
2023-11-23 08:43:21 +0100

    updating pipeline

--- stack-pulumi/pulumi/index.ts
+++ stack-pulumi/pulumi/index.ts
@@ -1,12 +1,15 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as resources from "@pulumi/azure-native/resources";
 import * as storage from "@pulumi/azure-native/storage";
+let config = new pulumi.Config();
+let rg_name = config.require("rg_name");
+let sa_name = config.require("sa_name");
 
 // Create an Azure Resource Group
-const resourceGroup = new resources.ResourceGroup("resourceGroup");
+const resourceGroup = new resources.ResourceGroup(rg_name);
 
 // Create an Azure resource (Storage Account)
-const storageAccount = new storage.StorageAccount("sa", {
+const storageAccount = new storage.StorageAccount(sa_name, {
     resourceGroupName: resourceGroup.name,
     sku: {
         name: storage.SkuName.Standard_LRS,
